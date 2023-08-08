d99db1e1a5e6c12e11af20f4ddab3e5e3f74da65
tashod peterson <tashodpeterson@tashods-Air.lan>
2023-08-08 18:17:24 -0400

    first

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,44 @@ import Pagination from "./Pagination";
 function App() {
   const [charactersList, setCharactersList] = useState([]);
   const [url, setUrl] = useState("https://swapi.dev/api/people/");
+  // I need to filter out the species and the planets?
+  // I do not know if I have to change the url for yhe planets
+
+  // const charactersArray = [
+  //   {
+  //     name: "Luke Skywalker",
+  //     homeworld: "http://www.getthehomeworld.com/1",
+  //     species: "",
+  //   },
+  //   {
+  //     name: "R2D2",
+  //     homeworld: "http://www.getthehomeworld.com/2",
+  //     species: "Robot",
+  //   },
+  // ]
+
+  // for species check to see if its an empyu array if so its a human if not add the url
 
   useEffect(() => {
     axios
       .get(url)
       .then((res) => {
-        console.log(res.data);
-        setCharactersList(res.data.results);
+        // console.log(res.data);
+        const charactersArray = res.data.results;
+
+        for (let character in charactersArray) {
+          const homeWorldUrl = character.homeworld;
+
+           axios.get(homeWorldUrl)
+          .then((res) => {
+            console.log(res.data);
+            character.homeworld = res.data.name
+          });
+
+          
+        }
+
+        setCharactersList(charactersArray);
       })
 
       .catch((err) => console.log(err));
@@ -26,7 +57,10 @@ function App() {
     <>
       <h1>Star Wars Search</h1>
       <Search url={url} setTypeGetData={setCharactersList} setUrl={setUrl} />
-      <Table setCharactersList={setCharactersList} charactersList={charactersList} />
+      <Table
+        setCharactersList={setCharactersList}
+        charactersList={charactersList}
+      />
       <Pagination setUrl={setUrl} setCharactersList={setCharactersList} />
     </>
   );
